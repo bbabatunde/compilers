21774dcee0f71c09a11c66692e4510f5a83f79bf
Amogh Pradeep <amoghbl1@gmail.com>
2019-03-17 17:51:59 -0400

    Fix function transform in desugar

--- egg-eater/compile.ml
+++ egg-eater/compile.ml
@@ -405,12 +405,14 @@ let rec tupArgs arg_lst =
     | [] -> []
 ;;
 
-let rec nonTups arg_lst =
+let rec replaceTups arg_lst ctr =
+    let tmp_tup_name = (sprintf "desugar_args_%d" ctr) in
     match arg_lst with
     | first::rest ->
         (match first with
-        | BTuple(_, _) -> tupArgs rest
-        | _ -> first :: (tupArgs rest))
+        | BTuple(_, loc) -> BName(tmp_tup_name, bind_to_typ first, loc) :: (replaceTups rest (ctr - 1))
+        | BName(_, _, _) -> first :: (replaceTups rest (ctr - 1))
+        | _ -> raise (InternalCompilerError "BBlank in function definition o.O"))
     | [] -> []
 ;;
 
@@ -482,7 +484,7 @@ let desugar (p : sourcespan program) : sourcespan program =
       | _ -> raise (InternalCompilerError "Processing args broken o.O"))
     | _ -> raise (InternalCompilerError "tupToName or tupArgs broken O.o")
   and helpArgs args =
-    let new_args = nonTups args in
+    let new_args = (replaceTups args (List.length args)) in
     let new_bindings = List.map2 helpNewBinds (tupArgs args) (tupToName args) in
     (new_args, new_bindings)
   and helpD (d : sourcespan decl) (* other parameters may be needed here *) =
@@ -490,7 +492,7 @@ let desugar (p : sourcespan program) : sourcespan program =
     | DFun(name, args, scheme, body, pos) ->
         let (new_args, new_binds) = helpArgs args in
         let new_body = ELet(new_binds, (helpE body), pos) in
-        DFun(name, new_args, scheme, new_body, pos)
+        DFun(name, new_args, scheme, helpE new_body, pos)
   and helpT (t : sourcespan typ) (* other parameters may be needed here *) =
     Error([NotYetImplemented "Implement desrugaring for types"])
   and helpS (s : sourcespan scheme) (* other parameters may be needed here *) =
